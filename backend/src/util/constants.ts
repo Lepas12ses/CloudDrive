cebf3fe1909a30f0a5098323af9049a13bc90286
Amogus <nomail@gus.com>
2025-10-16 17:07:36 +0300

    Changed uploads to user_files

--- backend/src/util/constants.ts
+++ backend/src/util/constants.ts
@@ -1,5 +1,5 @@
 import path from "path";
 
 export const ROOT_PATH = process.cwd();
-export const UPLOADS = "uploads";
+export const UPLOADS = "user_files";
 export const UPLOADS_PATH = path.join(ROOT_PATH, UPLOADS);
