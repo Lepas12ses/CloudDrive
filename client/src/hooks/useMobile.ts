e7e3978319e5e3a52f703ef240877c39cad1fdbe
Amogus <nomail@gus.com>
2025-11-10 14:19:48 +0300

    Setting isMobile state directly in useState hook

--- client/src/hooks/useMobile.ts
+++ client/src/hooks/useMobile.ts
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
 
+const mediaQuery = window.matchMedia(
+	"screen and (hover: none) and (pointer: coarse)"
+);
+
 export default function useMobile() {
-	const [isMobile, setIsMobile] = useState(false);
+	const [isMobile, setIsMobile] = useState(mediaQuery.matches);
 
 	useEffect(() => {
-		const mediaQuery = window.matchMedia(
-			"screen and (hover: none) and (pointer: coarse)"
-		);
-
-		setIsMobile(mediaQuery.matches);
-
 		function handleChange(e: MediaQueryListEvent) {
 			if (e.matches) setIsMobile(true);
 			else setIsMobile(false);
