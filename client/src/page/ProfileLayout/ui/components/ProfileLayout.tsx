b10a06c51786bb4a4506c75fd8f4543262376a7f
Amogus <nomail@gus.com>
2025-12-03 17:27:34 +0300

    Removed greeter from Profile layout

--- client/src/page/ProfileLayout/ui/components/ProfileLayout.tsx
+++ client/src/page/ProfileLayout/ui/components/ProfileLayout.tsx
@@ -5,10 +5,7 @@ const ProfileLayout: FC = () => {
 	return (
 		<div className=' flex flex-col items-center pt-15'>
 			<div className='min-w-100'>
-				<p>С возвращением!</p>
-				<div className='pt-5 mt-5 border-t border-t-(--border)'>
-					<Outlet />
-				</div>
+				<Outlet />
 			</div>
 		</div>
 	);
