13d48bcbbfa87ced8e1e899e21cbc2bc7fd22dc5
Amogus <nomail@gus.com>
2025-12-06 11:50:48 +0300

    Added jwt helper using jose

---
+++ backend/src/shared/lib/helper/jwt.ts
@@ -0,0 +1,40 @@
+import * as jose from "jose";
+import env from "../env.js";
+
+const signSecret = new TextEncoder().encode(env.jwtAccessSecret);
+const encryptionSecret = jose.base64url.decode(
+	jose.base64url.encode(env.jwtRefreshSecret)
+);
+
+export async function signJwt(
+	payload: object,
+	expires: number | string | Date
+) {
+	return await new jose.SignJWT({ ...payload })
+		.setProtectedHeader({ alg: "HS256" })
+		.setIssuedAt()
+		.setExpirationTime(expires)
+		.sign(signSecret);
+}
+
+export async function verifyJwt(jwt: string) {
+	return await jose.jwtVerify(jwt, signSecret);
+}
+
+export async function encryptJwt(
+	payload: object,
+	expires: number | string | Date
+) {
+	return await new jose.EncryptJWT({ ...payload })
+		.setProtectedHeader({
+			alg: "dir",
+			enc: "A128CBC-HS256",
+		})
+		.setIssuedAt()
+		.setExpirationTime(expires)
+		.encrypt(encryptionSecret);
+}
+
+export async function decryptJwt(jwt: string) {
+	return await jose.jwtDecrypt(jwt, encryptionSecret);
+}
