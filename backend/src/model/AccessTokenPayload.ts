0d19a14fd12cac81211afca5e260161fd6525848
Amogus <nomail@gus.com>
2025-12-07 10:43:03 +0300

    Changed user id to number type

--- backend/src/model/AccessTokenPayload.ts
+++ backend/src/model/AccessTokenPayload.ts
@@ -1,11 +1,11 @@
 export interface AccessTokenPayload {
-	userId: string;
+	userId: number;
 }
 
 export function isAccessTokenPayload(obj: unknown): obj is AccessTokenPayload {
 	if (typeof obj !== "object") return false;
 
-	if (!("userId" in obj) || typeof obj.userId !== "string") return false;
+	if (!("userId" in obj) || typeof obj.userId !== "number") return false;
 
 	return true;
 }
