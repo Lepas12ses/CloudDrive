d0852a41da7c95d28e17567c0d1da8087ae67e30
Amogus <nomail@gus.com>
2025-12-14 11:56:16 +0300

    Fixed repeat helper

--- client/src/shared/lib/helper/repeat.ts
+++ client/src/shared/lib/helper/repeat.ts
@@ -1,3 +1,3 @@
 export default function repeat<T>(count: number, fn: (index: number) => T) {
-	return new Array(count).fill(0).map(index => fn(index));
+	return new Array(count).fill(0).map((_, index) => fn(index));
 }
