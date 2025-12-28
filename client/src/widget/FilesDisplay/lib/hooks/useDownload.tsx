34500a84e1901f484940ab232f5f8a47df011d92
Amogus <nomail@gus.com>
2025-12-28 14:43:07 +0300

    Changed toasts logic in useDownload hook

--- client/src/widget/FilesDisplay/lib/hooks/useDownload.tsx
+++ client/src/widget/FilesDisplay/lib/hooks/useDownload.tsx
@@ -4,12 +4,32 @@ import type { File } from "@/entity/File";
 import downloadFile from "@/shared/lib/helper/downloadFile";
 import queryClient from "@/shared/api/queryClient";
 import fetchFileBlob from "../../api/downloadFile";
-import { useEffect, useEffectEvent } from "react";
+import { useCallback, useEffect, useEffectEvent } from "react";
 import useToast from "@/shared/lib/hooks/useToast";
 import LoadingSpinner from "@/shared/ui/components/LoadingSpinner/LoadingSpinner";
 import type ApiErrorResponse from "@/shared/model/ApiErrorResponse";
 
 export default function useDownload() {
+	const toast = useToast();
+
+	const showDownloadSuccessToast = useCallback(() => {
+		toast.open(
+			<div className='flex gap-3 items-center'>
+				<p>Файл успешно загружен</p>
+			</div>,
+			{ type: "success", dismissTime: 4000 }
+		);
+	}, [toast]);
+
+	const showErrorToast = useCallback(() => {
+		toast.open(
+			<div className='flex gap-3 items-center'>
+				<p>Не удалось скачать файл (</p>
+			</div>,
+			{ type: "error", dismissTime: 4000 }
+		);
+	}, [toast]);
+
 	const { isError, error, isPending, mutate } = useMutation<
 		Blob,
 		ApiErrorResponse,
@@ -19,11 +39,14 @@ export default function useDownload() {
 			mutationFn: (file: File) => fetchFileBlob(file.id),
 			onSuccess(data, file) {
 				downloadFile(data, file.name);
+				showDownloadSuccessToast();
+			},
+			onError() {
+				showErrorToast();
 			},
 		},
 		queryClient
 	);
-	const toast = useToast();
 
 	function proceed(file: File) {
 		mutate(file);
@@ -45,15 +68,6 @@ export default function useDownload() {
 		toast.close(id);
 	});
 
-	const showErrorToast = useEffectEvent(() => {
-		toast.open(
-			<div className='flex gap-3 items-center'>
-				<p>Не удалось скачать файл (</p>
-			</div>,
-			{ type: "error", dismissTime: 4000 }
-		);
-	});
-
 	useEffect(() => {
 		if (isPending) {
 			const id = showDownloadingToast();
@@ -65,14 +79,6 @@ export default function useDownload() {
 		// TODO: Надо как-то подружить eslint с useEffectEvent
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [isPending]);
-
-	useEffect(() => {
-		if (isError) {
-			showErrorToast();
-		}
-		// TODO: Надо как-то подружить eslint с useEffectEvent
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [isError]);
 
 	return {
 		isError,
